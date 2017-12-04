dd1bb56747e2fec4b40cc9791b31e6caffefd8b0
Gérard Rozsavolgyi <roza@univ-orleans.fr>
2017-12-04 10:16:53 +0100

    test with http-proxy

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ class OpenWeatherMap{
     constructor(APPID, units, {http_proxy = null } = {}) {
     this._APPID = APPID;
     this._units = units;
-    if (http_proxy ==null) this._proxy = null; 
+    if (http_proxy == null) this._proxy = null; 
     else this._proxy = new HttpProxyAgent(http_proxy);
   }
    getWeather(city){
      const urlow = new URL("http://api.openweathermap.org/data/2.5/weather"),
-        params = {q:city, APPID: this._APPID, units: this._units}
+        params = {q:city, APPID: this._APPID, units: this._units};
      Object.keys(params).forEach(key => urlow.searchParams.append(key, params[key]))
      let options = {};
      if (this._proxy != null) options = { agent: this._proxy};
--- test-ow-api.js
+++ test-ow-api.js
@@ -1,7 +1,8 @@
 let { OpenWeatherMap } = require('./index.js');
 const { API_KEY } = require('./ow-api-key.js');
 console.log(API_KEY);
-let ow = new OpenWeatherMap(API_KEY, 'metric');
+let ow = new OpenWeatherMap(API_KEY, 'metric',
+    {http_proxy:'http://wwwcache.univ-orleans.fr:3128'});
 
  ow.getWeather('Yakutsk').then(data => {
     console.log('Temp:', data.main.temp);
